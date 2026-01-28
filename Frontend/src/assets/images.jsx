9a51a74df1b7a0cc51cadc077c477037d9fab52e
Dipanshu Sandhaki <dipanshuroy6@gmail.com>
2026-01-28 23:56:57 +0530

    Fix images.jsx

--- Frontend/src/assets/images.jsx
+++ Frontend/src/assets/images.jsx
@@ -1,9 +1,10 @@
 import Logo from "./UULogomain.png";
 import PC from "./PC.png";
-import UULogo from "./UULogo.png";
+import UULogo from "./UULogo.png"; 
 import Quizlogo from "./Quizlogo.png";
 import ITUtsav from "./ITUtsavLogo.png";
 import LandingBG from "./landingBG.svg";
+import Itquiz from "./logo2.png";
 import thank from "./thank.png";
 import login from "./LoginMain.png";
 
@@ -11,6 +12,7 @@ const images = {
   Logo,
   PC,
   UULogo,
+  Itquiz,
   Quizlogo,
   ITUtsav,
   LandingBG,
