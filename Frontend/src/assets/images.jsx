c2d2fb3f87c2916972fb5a93c21d7c7197658220
Dipanshu Sandhaki <dipanshuroy6@gmail.com>
2026-01-28 23:52:41 +0530

    Fix images.jsx imports

--- Frontend/src/assets/images.jsx
+++ Frontend/src/assets/images.jsx
@@ -1,10 +1,9 @@
 import Logo from "./UULogomain.png";
 import PC from "./PC.png";
-import UULogo from "./UULogo.png"; 
+import UULogo from "./UULogo.png";
 import Quizlogo from "./Quizlogo.png";
 import ITUtsav from "./ITUtsavLogo.png";
 import LandingBG from "./landingBG.svg";
-import Itquiz from "./logo2.png";
 import thank from "./thank.png";
 import login from "./LoginMain.png";
 
@@ -12,7 +11,6 @@ const images = {
   Logo,
   PC,
   UULogo,
-  Itquiz,
   Quizlogo,
   ITUtsav,
   LandingBG,
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173, // or any port you prefer
+    port: 5173, 
   }
 })
